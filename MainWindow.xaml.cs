beb6d5c99c09af2ab660e25dd08488a477f870a0
msanru2019 <51949118+msanru2019@users.noreply.github.com>
2019-07-12 16:23:23 +0100

    n

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using MahApps.Metro.Behaviours;
+
+namespace MetroWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        static List<Customer> customers;
+        static List<Customer> retcustomers;
+        static List<Supplier> suppliers;
+        static List<Order> orders;
+        List<string> cities;
+
+        Supplier supplier;
+        Customer customer;
+        Order order;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+        void Initialize()
+        {
+            using (var db = new NorthwindEntities())
+            {
+                customers = db.Customers.ToList();
+                cities = (from c in db.Customers select c.City).Distinct().ToList();
+
+                suppliers = db.Suppliers.ToList();
+                orders = db.Orders.ToList();
+            }
+
+            ListViewCustomers.ItemsSource = customers;
+            ListViewSupplier.ItemsSource = suppliers;
+            ListViewOrder.ItemsSource = orders;
+            //ComoboxCity.ItemsSource = cities;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Menu _ver = new Menu();
+            this.Close();
+            _ver.ShowDialog();
+        }
+    }
+}
---
+++ Menu.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using MahApps.Metro.Behaviours;
+
+namespace MetroWpf
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : MetroWindow
+    {
+        static List<Customer> customers;
+        static List<Customer> retcustomers;
+        static List<Supplier> suppliers;
+        static List<Order> orders;
+        List<string> cities;
+
+        Supplier supplier;
+        Customer customer;
+        Order order;
+        public Menu()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+        void Initialize()
+        {
+            using (var db = new NorthwindEntities())
+            {
+                customers = db.Customers.ToList();
+                cities = (from c in db.Customers select c.City).Distinct().ToList();
+
+                suppliers = db.Suppliers.ToList();
+                orders = db.Orders.ToList();
+            }
+
+            ListViewCustomers.ItemsSource = customers;
+            ListViewSupplier.ItemsSource = suppliers;
+            ListViewOrder.ItemsSource = orders;
+            //ComoboxCity.ItemsSource = cities;
+        }
+        private void Tile_Click(object sender, RoutedEventArgs e)
+        {
+            FlyData.IsOpen = true;
+        }
+
+        private void BtnNext_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow _ver = new MainWindow();
+            this.Close();
+            _ver.ShowDialog();
+        }
+    }
+}
